cd93d0b495e10053f8d2f0ea5a9e1fe40bdd4bc0
Noah Walker <93171964+nwalker7727@users.noreply.github.com>
2024-05-02 08:08:38 -0700

    simple updates during playtesting

--- Assets/Scripts/FinalDay.cs
+++ Assets/Scripts/FinalDay.cs
@@ -10,11 +10,13 @@ public class FinalDay : MonoBehaviour
     // Start is called before the first frame update
     public void solution(){
         if(gameObject == sol1 && InventoryCount.rope > 0){
+            InventoryCount.rope -= 1;
             sol1.SetActive(false);
             sol2.SetActive(false);
             traveler.SetActive(true);
         }
         if(gameObject == sol2 && InventoryCount.sticks >= 8){
+            InventoryCount.sticks -= 8;
             sol1.SetActive(false);
             sol2.SetActive(false);
             traveler.SetActive(true);
